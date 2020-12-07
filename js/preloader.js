cba26a66fd1f6b3b8ed7cce9ed194ea2181b766c
nastyaR <maxshtl201@gmail.com>
2020-12-07 22:49:41 +0300

    PRELOAD

--- js/preloader.js
+++ js/preloader.js
@@ -3,10 +3,11 @@ function loadData() {
         setTimeout(resolve, 2000);
     })
 }
-
-loadData()
-    .then(() => {
-        let preloaderEl = document.getElementById('preloader');
-        preloaderEl.classList.add('hidden');
-        preloaderEl.classList.remove('visible');
-    });
+if (!window.onload){
+    loadData()
+        .then(() => {
+            let preloaderEl = document.getElementById('preloader');
+            preloaderEl.classList.add('hidden');
+            preloaderEl.classList.remove('visible');
+        });
+}
